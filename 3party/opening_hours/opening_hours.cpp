e65b3762ef04d4f21addd3e3aafd2b140700fe88
Raghad Dahi <raghaddahi27@gmail.com>
2025-10-29 05:51:10 +0300

    [3party] Add support for public holidays in opening_hours lib
    Signed-off-by: Raghad Dahi <raghaddahi27@gmail.com>

--- 3party/opening_hours/opening_hours.cpp
+++ 3party/opening_hours/opening_hours.cpp
@@ -883,6 +883,17 @@ OpeningHours::OpeningHours(TRuleSequences const & rule):
 {
 }
 
+OpeningHours::OpeningHours(std::string const & rule, THolidayDates const & holidays)
+  : m_valid(Parse(rule, m_rule))
+  , m_holidays(holidays)
+{}
+
+OpeningHours::OpeningHours(TRuleSequences const & rule, THolidayDates const & holidays)
+  : m_rule(rule)
+  , m_valid(true)
+  , m_holidays(holidays)
+{}
+
 bool OpeningHours::IsOpen(time_t const dateTime) const
 {
   return osmoh::IsOpen(m_rule, dateTime);
@@ -902,6 +913,10 @@ OpeningHours::InfoT OpeningHours::GetInfo(time_t const dateTime) const
 {
   InfoT info;
   info.state = GetState(m_rule, dateTime);
+
+  if (m_holidays.contains(dateTime))
+    info.isHoliday = true;
+
   if (info.state != RuleState::Unknown)
   {
    if (info.state == RuleState::Open)
